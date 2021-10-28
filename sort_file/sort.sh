670fdb3f2a94f2070040c1ccbc8638f94c15a7ab
BobLouis <bob900316@gmail.com>
2021-10-28 12:31:41 +0800

    update

---
+++ sort_file/sort.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd /Users/kounuki/Linux_course/mini_project/sort_file/compressed_files;
+mkdir -p zip;
+mkdir -p rar;
+mkdir -p tar.gz;
+mkdir -p unknown;
+for f in *.zip; do
+  mv ${f} zip;
+  unzip zip/${f};
+done
+cd zip;
+unzip "*.zip";
+cd ..;
+
+for f in *.tar.gz; do
+  mv ${f} tar.gz;
+done
+cd tar;
+for f in *.tar.gz; do 
+  tar xf "$f";
+done
+cd ..;
+
+for f in *.rar; do
+  mv ${f} rar;
+done
+
+find . -maxdepth 1 -type f -exec mv {} unknown \;
